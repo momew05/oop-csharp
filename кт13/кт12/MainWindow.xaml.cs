b577061c954994facd1d06b0acc686efe6dc10a5
вапр <kornenko_liza@mail.ru>
2025-11-14 17:41:36 +1000

    Добавлена кт 13 (папка кт12 в кт13 это кт 13)

---
+++ кт13/кт12/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace кт12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnOpenSecond_Click(object sender, RoutedEventArgs e)
+        {
+            var secondWindow = new SecondWindow();
+            secondWindow.Owner = this;
+            this.Hide();
+            bool? result = secondWindow.ShowDialog();
+            this.Show();
+            if (result == true && secondWindow.hello == true) 
+            {
+                UserInfoTextBlock.Text = $"{secondWindow.UserName}";
+            }
+            else if(result == true)
+            {
+                UserInfoTextBlock.Text = $"Привет, {secondWindow.UserName}!";
+            }
+            else
+            {
+                MessageBox.Show("Окно было закрыто без передачи данных");
+            }
+
+            if (result == true && secondWindow.emoji == true)
+            {
+                secret.Visibility = Visibility.Visible;
+            }
+
+
+        }
+    }
+}
---
+++ кт13/кт12/SecondWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace кт12
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    
+    public partial class SecondWindow : Window
+    {
+        public bool hello;
+        public bool emoji;
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string UserName { get; set; }
+        
+
+        private void OpenMain_Click(object sender, RoutedEventArgs e)
+        {
+            UserName = UserNameTextBox.Text;
+            this.DialogResult = true;
+            this.Close();
+            
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void UnableGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem Hello = (MenuItem)sender;         
+            hello = Hello.IsChecked; 
+            
+        }
+
+        private void AddEmoji_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem Emoji = (MenuItem)sender;
+            emoji = Emoji.IsChecked;
+            
+        }
+
+        private void CloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
